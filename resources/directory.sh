48d59e9523dcdcd0ee7973d66d8f3a2a70199856
Dismanc <44057412+Dismanc@users.noreply.github.com>
2018-11-20 17:09:26 +0100

    VOILLLLLLA

--- resources/directory.sh
+++ resources/directory.sh
@@ -1,5 +1,24 @@
-#!/bin/sh
-whereIAM=racine
+#!/bin/bash
+
+# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+#
+# Project        : Hack Recipe Coca-Cola
+# Name           : game-fscociety.sh
+# Version        : 1.0
+# Date created   : 30.10.2018
+#
+# Author         : Timothee Frily
+#                  Michel Cruz
+#
+# Purpose        : The user can play the Game fscociety
+#
+# Precondition   : Package pv
+#
+# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+
+WhereIAM=racine
 admin=administration
 
 
